df1281b34ba2e5d32fefbef7189737aebfd505b8
bernarducs <bernarducs@gmail.com>
2019-08-27 01:14:27 -0300

    Add files via upload

---
+++ grouping_dicts.py
@@ -0,0 +1,12 @@
+from itertools import groupby
+
+"""
+grouping a list of dicts by your key value
+"""
+
+lista = [
+        {'a': 1}, {'a': 5}, {'a': 3}, {'b': 2}, {'b': 9}
+    ]
+
+for key, group in groupby(lista, lambda key: list(key)):
+    print('{}: {}'.format(key[0], [i[key[0]] for i in group]))
