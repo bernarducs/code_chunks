02f91ed92d9aa920305fe8741387ff200da84b9c
bernarducs <bernarducs@gmail.com>
2019-08-27 01:14:11 -0300

    Delete grouping_dicts.py

--- grouping_dicts.py
+++
@@ -1,8 +0,0 @@
-from itertools import groupby
-
-lista = [
-        {'a': 1}, {'a': 5}, {'a': 3}, {'b': 2}, {'b': 9}
-    ]
-
-for key, group in groupby(lista, lambda key: list(key)):
-    print('{}: {}'.format(key[0], [i[key[0]] for i in group]))
